b113211aff31c32da4c000fcbb13d258555a6585
CheaYun <thcodus1421@gmail.com>
2022-04-19 14:22:53 +0900

    0419 프로젝트 추가

---
+++ 0419/src/sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("시작 숫자 : ");
+		int a = sc.nextInt();
+		System.out.print("마지막 숫자 : ");
+		int b = sc.nextInt();
+		int sum = 0;
+		for(int i = a ; i <= b ; i++)
+		{
+			sum += i;
+		}
+		System.out.println(a + "부터 " + b + "까지의 합은 " + sum);
+	}
+
+}
